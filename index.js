d30054aef8b380a0b8ae69e679f38cf4b500a11c
Camille Emmer <camilleemmer@gmail.com>
2022-06-21 19:34:43 -0600

    commit tree 5669cb234f74f22408851b6a7fb15ad21a91e902

---
+++ index.js
@@ -0,0 +1,3 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+const square = (parameter2) => parameter2 * parameter2;
+const add = (parameter1, parameter2) => parameter1 + parameter2;
